ce4c7ad52f6b21b569369eafbab780808b12b463
Sangita Mane <sangita.mane@springernature.com>
2024-11-22 17:46:04 +0100

    node-fetch added for api fetch

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import apiFetch from '@wordpress/api-fetch';
+import fetch from 'node-fetch';
 
 
 const router = new express.Router();
@@ -17,17 +18,39 @@ router.get('/', (request, res) => {
 	});
 });
 
+const API_BASE_URL = 'https://h84zz5jyx27wb61qlhm4de16i.js.wpenginepowered.com/wp-json/wp/v2';
+
 // Example route to fetch posts
 router.get('/posts', async (req, res) => {
 	try {
-		const posts = await apiFetch({ path: '/wp/v2/posts' });
-		res.render('posts', {
-		  title: 'WordPress Posts',
-		  posts: posts
-		});
-	  } catch (error) {
-		res.status(500).json({ error: error.message });
+		const apiUrl = process.env.NEXT_PUBLIC_WORDPRESS_URL || API_BASE_URL;
+		const response = await fetch(`${apiUrl}/posts`);
+	  if (!response.ok) {
+		throw new Error(`Error fetching posts: ${response.statusText}`);
 	  }
-});
+	  const posts = await response.json();
+	  res.render('posts', { title: 'Blog Posts', posts });
+	} catch (error) {
+	  res.status(500).json({ error: error.message });
+	}
+  });
+  
+  // Fetch and display pages
+  router.get('/pages', async (req, res) => {
+	try {
+		const apiUrl = process.env.NEXT_PUBLIC_WORDPRESS_URL || API_BASE_URL;
+
+		const response = await fetch(`${apiUrl}/pages`);
+
+	  if (!response.ok) {
+		throw new Error(`Error fetching pages: ${response.statusText}`);
+	  }
+	  const pages = await response.json();
+	  res.render('pages', { title: 'Pages', pages });
+	} catch (error) {
+	  res.status(500).json({ error: error.message });
+	}
+  });
+  
 
 export {router};
