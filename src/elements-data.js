df05363fee2970f910f75a3a5ec550dce523d80e
Sangita Mane <sangita.mane@springernature.com>
2024-11-26 17:13:23 +0100

    footer data added

--- src/elements-data.js
+++ src/elements-data.js
@@ -5,6 +5,12 @@ const elementsData = {
 		  "src": "/img/logo-springernature-blue.svg",
 		  "alt": "Logo Header"
 		}
+	},
+    "eds-c-footer": {
+		"$schema": "http://json-schema.org/draft-07/schema#",
+        "$id": "https://springernature.com/eds-c-footer.schema.json",
+        "title": "Footer",
+        "description": "Footer component"
 	}
 };
 
